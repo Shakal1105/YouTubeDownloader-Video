9c4e9f2041340cc88a4c8540eda393ac6fa3008b
Shakal1105 <55797859+Shakal1105@users.noreply.github.com>
2022-08-29 23:00:36 +0300

    Add files via upload

---
+++ Lastversion/pytoexe.py
@@ -0,0 +1,3 @@
+from cx_Freeze import setup, Executable
+
+setup(name="UTybeLinkDownload", version="0.11.1",lang='Ukraine', description="[Shakal] Glory to Ukraine, .ussian warship fuckyou", executables=[Executable("YouTubeDownloadVideos.py")])
