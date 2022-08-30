4983d64b6bc8afd9dabcd02bcfb7763df95c6fca
Shakal1105 <55797859+Shakal1105@users.noreply.github.com>
2022-08-30 23:54:30 +0300

    Add files via upload

---
+++ Lastversion/pytoexe.py
@@ -0,0 +1,25 @@
+from cx_Freeze import setup, Executable
+import sys, tkinter.filedialog
+
+def setuper(base, filecompile):
+    setup(
+        name="UTybeLinkDownload",
+        icon="shak.ico",
+        version="0.11.1",
+        lang='Ukraine',
+        description="[Shakal] Glory to Ukraine, .ussian warship fuckyou",
+        executables=[Executable(filecompile, base=base, icon="shak.ico", shortcutDir="Shakal YoutubeLoader", trademarks="Shakal", shortcutName="ShakLoaderYoutube", copyright="The Shakal")]
+    )
+
+x32_x64 = sys.platform
+filer = tkinter.filedialog.askopenfilename(title="Файл setup.py")
+filecompile = tkinter.filedialog.askopenfilename(title="Файл которий в exe")
+
+if x32_x64 == 'win32':
+    base = "Win32GUI"
+    setuper(base=base, filecompile=filecompile)
+elif x32_x64 == "win64":
+    base = "Win64GUI"
+    setuper(base=base, filecompile=filecompile)
+else:
+    print("Error, this compile only x32 or x64 Windows")
